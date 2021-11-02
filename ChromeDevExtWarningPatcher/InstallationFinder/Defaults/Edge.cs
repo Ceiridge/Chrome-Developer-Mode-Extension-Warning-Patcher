4a33b1d19ceebbf444bff6b03faa71cc1627615d
Ceiridge <philipp34533@icloud.com>
2021-11-02 17:58:47 +0100

    Add auto install path for Edge downloaded from the MS Store Also increment the version

--- ChromeDevExtWarningPatcher/InstallationFinder/Defaults/Edge.cs
+++ ChromeDevExtWarningPatcher/InstallationFinder/Defaults/Edge.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ChromeDevExtWarningPatcher.InstallationFinder.Defaults { 
@@ -10,6 +11,7 @@ namespace ChromeDevExtWarningPatcher.InstallationFinder.Defaults {
 
 			AddDllAndExeToList(dllFiles, this.GetLatestDllAndExe(new DirectoryInfo(@"C:\Program Files (x86)\Microsoft\Edge\Application"), "msedge.dll", "msedge.exe"));
 			AddDllAndExeToList(dllFiles, this.GetLatestDllAndExe(new DirectoryInfo(@"C:\Program Files\Microsoft\Edge\Application"), "msedge.dll", "msedge.exe"));
+			AddDllAndExeToList(dllFiles, this.GetLatestDllAndExe(new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Microsoft\Edge\Application")), "msedge.dll", "msedge.exe")); // From the MS Store
 
 			return dllFiles;
 		}
